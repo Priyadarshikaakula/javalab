65ec3920a38f4f21965860b132027fc60ee2d29a
Priyadarshika Akula <25ag1a6601@gmail.com>
2026-08-27 08:55:09 +0000

    added

---
+++ program.java/LogicalOp.java
@@ -0,0 +1,10 @@
+public class LogicalOp
+{
+public static void main(String[] args)
+{
+int x=10;
+System.out.println(x==10 && x>=5); 
+System.out.println(x==10 || x>=5); 
+System.out.println ( ! ( x==10 ));
+}
+}
